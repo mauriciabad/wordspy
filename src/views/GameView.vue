47c512023e2c83a5c402a9650455c88ed1a2d4a3
mauriciabad <hello@mauriciabad.com>
2023-04-23 20:45:38 -0500

    roles desc

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -18,115 +18,63 @@ const hasData = computed<boolean>(
     getQueryParam('wordSetId') !== undefined ||
     getQueryParam('roleId') !== undefined
 )
-const {getWordSet} = useWordTranslations()
+const { getWordSet } = useWordTranslations()
 
 const roleId = computed<string | undefined>(() => getQueryParam('roleId'))
 const wordSetId = computed<string | undefined>(() => getQueryParam('wordSetId'))
 const wordId = computed<string | undefined>(() => getQueryParam('wordId'))
-const wordSet = computed(() =>getWordSet(Number(wordSetId.value) ?? 0))
-const word = computed<string | undefined>(() => wordSet.value.words[Number(wordId.value)??0])
+const wordSet = computed(() => getWordSet(Number(wordSetId.value) ?? 0))
+const word = computed<string | undefined>(
+  () => wordSet.value.words[Number(wordId.value) ?? 0]
+)
 
 const role = computed<string>(() => {
   switch (roleId.value) {
-    case "1": return t("ui2.role.potato")
-    case "2": return t("ui2.role.beetroot")
-    case "3": return t("ui2.role.rottenTurnip")
-    default: return "Unknown role"
+    case '1':
+      return t('ui2.role.potato')
+    case '2':
+      return t('ui2.role.beetroot')
+    case '3':
+      return t('ui2.role.rottenTurnip')
+    default:
+      return 'Unknown role'
   }
 })
-
 </script>
 
 <template>
   <CustomLayout locale-selector>
     <template v-if="hasData">
-          <h1>{{  roleId === '2' ? '???' : word }}</h1>
-          ---
-          <h1>{{ role }}</h1>
-      </template>
+      <h1 class="word">{{ roleId === '2' ? '???' : word }}</h1>
 
-    <template v-else>
-      No data
+      <h2 class="role" :class="[`role--${roleId}`]">
+        {{ t(`ui2.roles.${roleId}.name`) }}
+      </h2>
+      <p class="description">{{ t(`ui2.roles.${roleId}.description`) }}</p>
     </template>
+
+    <template v-else> No data </template>
   </CustomLayout>
 </template>
 
 <style scoped lang="scss">
-.name {
-  margin-bottom: 0.5rem;
-  font-size: 1.5rem;
+.word {
+  font-size: 4rem;
 }
 
-.ilustration {
-  --size: 100%;
+.role {
+  margin-top: 3rem;
 
-  display: inline-block;
-  width: 100%;
-  max-width: var(--size);
-  padding: 1rem;
-  border: 1px solid var(--vt-c-divider-light-2);
-  border-radius: 1rem;
-  aspect-ratio: 1;
-  background-color: #fff;
-  box-shadow: 0 20px 25px -5px rgb(0 0 0 / 10%),
-    0 8px 10px -6px rgb(0 0 0 / 10%);
-  cursor: pointer;
-  object-fit: contain;
-
-  &--ability {
-    --size: 5rem;
-
-    padding: 0.5rem;
-    border-radius: 0.5rem;
+  &--1 {
+    color: lightseagreen;
   }
 
-  &--character {
-    --size: #{5rem * 2 + 1rem};
+  &--2 {
+    color: royalblue;
   }
-}
 
-.ability-ilustrations-list {
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-  margin-top: 1rem;
-  gap: 1rem;
-}
-
-h1 {
-  margin-top: 0.5rem;
-}
-
-h2 {
-  margin-top: 2rem;
-}
-
-strong {
-  font-weight: bold;
-}
-
-.creation-date {
-  position: sticky;
-  top: 100%;
-  bottom: 0;
-  left: 0;
-  padding-top: 2rem;
-  font-size: 0.85rem;
-}
-
-.error-explanation {
-  margin-top: 2rem;
-}
-
-.error-list {
-  padding: 1rem;
-  padding-left: 2rem;
-  border: 1px solid var(--color-border);
-  border-radius: 0.5rem;
-  margin-top: 1rem;
-  background-color: var(--color-background-soft);
-  color: var(--color-red);
-  text-align: left;
+  &--3 {
+    color: crimson;
+  }
 }
 </style>
