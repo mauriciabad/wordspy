6b2a3031530eb40d402cdcc076c48e08a832e06e
mauriciabad <hello@mauriciabad.com>
2023-05-06 19:43:26 -0500

    Remove styles

--- src/components/HelpButton.vue
+++ src/components/HelpButton.vue
@@ -26,75 +26,9 @@ watch(showHelpModal, () => {
 </script>
 
 <template>
-  <IconButton class="button button--help-modal" @click="showHelpModal = true">
+  <IconButton @click="showHelpModal = true">
     <template #icon><QuestionMarkCircleIcon /></template>
     {{ t('ui.help') }}
     <HelpModal v-model:modelValue="showHelpModal" />
   </IconButton>
 </template>
-
-<style lang="scss">
-.link {
-  display: inline-block;
-  padding: 0.5rem 1.125rem;
-  border: 1px solid var(--color-border);
-  border-radius: 0.5rem;
-  background-color: var(--color-background-soft);
-  box-shadow: 0 0 0 0 var(--color-primary);
-  cursor: pointer;
-  font-size: 1rem;
-  justify-self: center;
-  outline: none;
-  text-align: center;
-  transition: box-shadow 0.2s cubic-bezier(0.18, 0.89, 0.32, 1.28);
-
-  &--disabled {
-    cursor: not-allowed;
-    opacity: 0.5;
-  }
-
-  &:focus-visible {
-    box-shadow: 0 0 0 3px var(--color-primary);
-  }
-
-  > svg {
-    display: inline-block;
-    height: 20px;
-    margin-right: 0.5rem;
-    vertical-align: -0.25em;
-  }
-
-  &--main {
-    position: sticky;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    width: calc(100% + 2 * var(--main-padding, 1rem));
-    max-width: calc(var(--max-width, 30rem) + 2rem);
-    padding: 0.75rem 0.5rem;
-    border: 0;
-    border-radius: 0.5rem 0.5rem 0 0;
-    margin: calc(-1 * var(--main-padding, 1rem));
-    margin-top: 1rem;
-    background-color: var(--color-primary);
-    box-shadow: 0 0 16px 0 rgb(0 0 0 / 20%);
-    color: #fff;
-    font-size: 1.25rem;
-    font-weight: 600;
-    letter-spacing: 0.1em;
-    text-transform: uppercase;
-
-    > svg {
-      height: 24px;
-    }
-
-    &:focus-visible {
-      box-shadow: 0 0 0 3px var(--color-text);
-    }
-
-    @media (max-width: 32rem) {
-      border-radius: 0;
-    }
-  }
-}
-</style>
