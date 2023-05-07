1bb53f11f64c8eea2f0c53110e30d021a00a0c1f
mauriciabad <hello@mauriciabad.com>
2023-05-07 15:30:53 -0600

    Reorganize dist

--- cypress/e2e/help-dialog.spec.ts
+++ cypress/e2e/help-dialog.spec.ts
@@ -71,37 +71,37 @@ describe('Help dialog', () => {
     cy.contains('Rules').should('not.exist')
     urlShouldEqual('/?gameRound=9808&wordSetId=5&playerNumber=empty')
   })
-})
 
-it('from the game view', () => {
-  cy.visit('/game?roleId=chaos&wordSetId=4&wordId=1')
+  it('from the game view', () => {
+    cy.visit('/game?roleId=chaos&wordSetId=4&wordId=1')
 
-  // Closed by default
-  cy.contains('Rules').should('not.exist')
-  urlShouldEqual('/game?roleId=chaos&wordSetId=4&wordId=1')
+    // Closed by default
+    cy.contains('Rules').should('not.exist')
+    urlShouldEqual('/game?roleId=chaos&wordSetId=4&wordId=1')
 
-  // Opens dialog
-  cy.contains('Help').click()
-  cy.contains('Rules')
-  urlShouldEqual('/game?roleId=chaos&wordSetId=4&wordId=1&showHelpModal=true')
+    // Opens dialog
+    cy.contains('Help').click()
+    cy.contains('Rules')
+    urlShouldEqual('/game?roleId=chaos&wordSetId=4&wordId=1&showHelpModal=true')
 
-  // Closes dialog
-  cy.contains('Close').click()
-  cy.contains('Help')
-  cy.contains('Rules').should('not.exist')
-  urlShouldEqual('/game?roleId=chaos&wordSetId=4&wordId=1')
+    // Closes dialog
+    cy.contains('Close').click()
+    cy.contains('Help')
+    cy.contains('Rules').should('not.exist')
+    urlShouldEqual('/game?roleId=chaos&wordSetId=4&wordId=1')
 
-  // ------------------------
-  // When scanning a QR code with other data and showHelpModal true
-  cy.visit('/game?roleId=chaos&wordSetId=4&wordId=1&showHelpModal=true')
+    // ------------------------
+    // When scanning a QR code with other data and showHelpModal true
+    cy.visit('/game?roleId=chaos&wordSetId=4&wordId=1&showHelpModal=true')
 
-  // Opens dialog
-  cy.contains('Rules')
-  urlShouldEqual('/game?roleId=chaos&wordSetId=4&wordId=1&showHelpModal=true')
+    // Opens dialog
+    cy.contains('Rules')
+    urlShouldEqual('/game?roleId=chaos&wordSetId=4&wordId=1&showHelpModal=true')
 
-  // Closes dialog
-  cy.contains('Close').click()
-  cy.contains('Help')
-  cy.contains('Rules').should('not.exist')
-  urlShouldEqual('/game?roleId=chaos&wordSetId=4&wordId=1')
+    // Closes dialog
+    cy.contains('Close').click()
+    cy.contains('Help')
+    cy.contains('Rules').should('not.exist')
+    urlShouldEqual('/game?roleId=chaos&wordSetId=4&wordId=1')
+  })
 })
