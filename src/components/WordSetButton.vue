3f1eedb91523b654168cc34f5e5417c590207a5a
mauriciabad <hello@mauriciabad.com>
2023-05-14 12:14:49 -0600

    Add aria to wordsetbutton

--- src/components/WordSetButton.vue
+++ src/components/WordSetButton.vue
@@ -3,12 +3,20 @@ import { InformationCircleIcon } from '@heroicons/vue/outline'
 import { ref } from 'vue'
 import IconButton from '@/components/IconButton.vue'
 import WordSetModal from '@/components/WordSetModal.vue'
+import { useI18n } from 'vue-i18n'
 
 const showWordSetModal = ref<boolean>(false)
+
+const { t } = useI18n()
 </script>
 
 <template>
-  <IconButton big @click="showWordSetModal = true">
+  <IconButton
+    big
+    :title="t('ui.wordSetDetails')"
+    :aria-label="t('ui.wordSetDetails')"
+    @click="showWordSetModal = true"
+  >
     <template #icon><InformationCircleIcon /></template>
     <WordSetModal v-model:modelValue="showWordSetModal" />
   </IconButton>
